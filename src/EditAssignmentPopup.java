96ea950559f94f75aa7f1f187743975761a96550
Joe Fischetti <jkfischetti@gmail.com>
2017-04-26 20:26:46 -0400

    Version 1.5

--- src/EditAssignmentPopup.java
+++ src/EditAssignmentPopup.java
@@ -1,3 +1,6 @@
+//This is the frame used for editing assignment details
+//
+
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import java.awt.GridLayout;
@@ -14,37 +17,63 @@ public class EditAssignmentPopup extends JFrame{
 
 	private static final long serialVersionUID = 1L;
 
+	//These are the panels required for the frame.
+	//
 	private JPanel mainPanel, leftPanel, rightPanel;
 	
+	//This is the button used for submitting the changes
+	//
 	private JButton btnSubmit;
 	
+	//These are the jtextfields used for editing the information
+	//
 	private JTextField txtAssignmentName, txtTotalPoints, txtDescription, course, assignmentType;
 	
+	//An assignment object, returned for database modification
+	//
 	Assignment assignment;
-	
-	public EditAssignmentPopup(	DefaultListModel<Course> courseList, 
-								DefaultListModel<AssignmentType> assignmentTypes,
-								Assignment inputAssignment){
 		
+	//This is the constructor for the frame
+	//
+	public EditAssignmentPopup(	DefaultListModel<Course> courseList, 
+					DefaultListModel<AssignmentType> assignmentTypes,
+					Assignment inputAssignment){
+		
+		//Set the local assignment variable to be equal to the one that was passed
+		//	This will allow me to populate the fields
+		//
 		assignment = inputAssignment;
 		
+		//Create the jpanels
+		//
 		mainPanel = new JPanel();
 		leftPanel = new JPanel();
 		rightPanel = new JPanel();
-				
+		
+		//Create the jtextfields and set their values to the values from the assignment
+		//
 		txtAssignmentName = new JTextField(assignment.getName());
 		txtAssignmentName.setEnabled(false);
 		txtTotalPoints = new JTextField(assignment.getPoints() + "");
 		txtDescription = new JTextField(assignment.getDescription());
-		
 		course = new JTextField(assignment.getCourse());
+		
+		//Disable the course editing field so that it doesn't get modified
+		//
 		course.setEnabled(false);
 		
+		//Set the assignment type field
+		//
 		assignmentType = new JTextField(assignment.getType() + "");
 		assignmentType.setEnabled(false);
 		
+		//Submit button
+		//
 		btnSubmit = new JButton("Submit Changes");
 		
+		//Add all of the elements to the panels
+		//Left panel
+		//
 		leftPanel.setLayout(new GridLayout(0,1));
 		leftPanel.add(new JLabel("Assignment Name:"));
 		leftPanel.add(new JLabel("Course:"));
@@ -52,6 +81,8 @@ public class EditAssignmentPopup extends JFrame{
 		leftPanel.add(new JLabel("Total Points:"));
 		leftPanel.add(new JLabel("Description"));
 		
+		//right panel
+		//
 		rightPanel.setLayout(new GridLayout(0,1));
 		rightPanel.add(txtAssignmentName);
 		rightPanel.add(course);
@@ -59,25 +90,30 @@ public class EditAssignmentPopup extends JFrame{
 		rightPanel.add(txtTotalPoints);
 		rightPanel.add(txtDescription);
 		
-		
+		//Main panel that holds both panels
+		//
 		mainPanel.setLayout(new GridLayout(0,2));
 		mainPanel.add(leftPanel);
 		mainPanel.add(rightPanel);
 		
-		
+		//Set the window properties
+		//
 		this.setBounds(250, 250, 640, 480);
 		this.setTitle("Modify Assignment");
 		this.setLayout(new BorderLayout());
 		this.add(mainPanel, BorderLayout.CENTER);
 		this.add(btnSubmit, BorderLayout.SOUTH);
 		
+		//Make it visible
+		//
 		this.setVisible(true);
 	}
 	
+	//Public setter method for the button action.  This will be passed from the main program
+	//
 	public void submitActionListener(ActionListener sal){
 		btnSubmit.addActionListener(sal);
 	}
-	
 	
 	//Create a new student from the provided information with a matching id_num
 	//	from the student provided
