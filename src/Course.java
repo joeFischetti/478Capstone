2833d9c4e8345e7e6477847bf02db469d272b143
Joe Fischetti <jkfischetti@gmail.com>
2017-04-26 20:19:31 -0400

    Version 1.5

--- src/Course.java
+++ src/Course.java
@@ -1,44 +1,46 @@
+//This is a class for the course object
+//
 
 public class Course{
 
-		private String course_num, name, description, day;
-		private int max_students;
+	private String course_num, name, description, day;
+	private int max_students;
 		
-		public Course(String num){
-			course_num = new String(num);
-		}
+	public Course(String num){
+		course_num = new String(num);
+	}
 		
-		public Course(String num, String newName, String newDescription, String max, String newDay){
-			course_num = new String(num);
-			name = new String(newName);
-			description = new String(newDescription);
-			day = new String(newDay);
-			max_students = Integer.parseInt(max);
-		}
+	public Course(String num, String newName, String newDescription, String max, String newDay){
+		course_num = new String(num);
+		name = new String(newName);
+		description = new String(newDescription);
+		day = new String(newDay);
+		max_students = Integer.parseInt(max);
+	}
 		
-		public String toString(){
-			return course_num;
-		}
 	
-		public String getNUM(){
-			return course_num;
-		}
-		
-		public String getName(){
-			return name;
-		}
-		
-		public String getDesc(){
-			return description;
-		}
-		
-		public String getDay(){
-			return day;
-		}
-		
-		public int getMax(){
-			return max_students;
-		}
+	//Getter and setter methods
+	public String toString(){
+		return course_num;
+	}
 
-
+	public String getNUM(){
+		return course_num;
+	}
+	
+	public String getName(){
+		return name;
+	}
+		
+	public String getDesc(){
+		return description;
+	}
+		
+	public String getDay(){
+		return day;
+	}
+		
+	public int getMax(){
+		return max_students;
+	}
 }
