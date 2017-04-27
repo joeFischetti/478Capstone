d6d8cdca3219d9310494d61b7827502bddb000ad
Joe Fischetti <jkfischetti@gmail.com>
2017-04-26 20:56:54 -0400

    Version 1.5

--- src/EditStudentPopup.java
+++ src/EditStudentPopup.java
@@ -1,3 +1,6 @@
+//This is the popup window used for editing students
+//
+
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import java.awt.GridLayout;
@@ -12,22 +15,36 @@ public class EditStudentPopup extends JFrame{
 
 	private static final long serialVersionUID = 1L;
 
+	//Create the jpanels for different regions
+	//
 	private JPanel mainPanel, leftPanel, rightPanel;
 	
+	//Create the submit button
+	//
 	private JButton btnSubmit;
 	
+	//Create the jtext fields for the various pieces of information that will be displayed
+	//
 	private JTextField txtFirstName, txtLastName, txtAddress, txtCity, txtState, txtZip, txtDOB;
 	
+	//Create a student object which will be returned after the user has made their
+	// changes
 	private Student student;
 	
 	public EditStudentPopup(Student existingStudent){
 		
+		//Set the local student variable equal to the values that were passed to it
+		//
 		student = existingStudent;
 		
+		//Init the jpanels
+		//
 		mainPanel = new JPanel();
 		leftPanel = new JPanel();
 		rightPanel = new JPanel();
-				
+		
+		//Init the text fields, using the values from the student variable
+		//
 		txtFirstName = new JTextField(student.getFirst());
 		txtLastName = new JTextField(student.getLast());
 		txtAddress = new JTextField(student.getAddress());
@@ -36,8 +53,12 @@ public class EditStudentPopup extends JFrame{
 		txtZip = new JTextField(student.getZip() + "");
 		txtDOB = new JTextField(student.getDOB());
 		
+		//init the button and make it say something
+		//
 		btnSubmit = new JButton("Submit Changes to student");
 		
+		//Set the panel layouts and add things to them
+		//
 		leftPanel.setLayout(new GridLayout(0,1));
 		leftPanel.add(new JLabel("First Name:"));
 		leftPanel.add(new JLabel("Last Name:"));
@@ -47,6 +68,8 @@ public class EditStudentPopup extends JFrame{
 		leftPanel.add(new JLabel("Zip:"));
 		leftPanel.add(new JLabel("Date of Birth:"));
 		
+		//More
+		//
 		rightPanel.setLayout(new GridLayout(0,1));
 		rightPanel.add(txtFirstName);
 		rightPanel.add(txtLastName);
@@ -56,11 +79,14 @@ public class EditStudentPopup extends JFrame{
 		rightPanel.add(txtZip);
 		rightPanel.add(txtDOB);
 		
+		//More
+		//
 		mainPanel.setLayout(new GridLayout(0,2));
 		mainPanel.add(leftPanel);
 		mainPanel.add(rightPanel);
 		
-		
+		//Init the frame, and make it visible
+		//
 		this.setBounds(250, 250, 640, 480);
 		this.setTitle("Edit Existing Student");
 		this.setLayout(new BorderLayout());
@@ -70,16 +96,23 @@ public class EditStudentPopup extends JFrame{
 		this.setVisible(true);
 	}
 	
+	//Make the submit button do things (passed from the main program
+	//
 	public void submitActionListener(ActionListener sal){
 		btnSubmit.addActionListener(sal);
 	}
 	
-	
-	//Create a new student from the provided information with a matching id_num
+	//Create and return a new student from the provided information with a matching id_num
 	//	from the student provided
 	//
 	public Student newStudent(){
-		return new Student(student.getID(), txtFirstName.getText(), txtLastName.getText(), txtAddress.getText(), txtCity.getText(),
-				txtState.getText(), txtZip.getText(), txtDOB.getText());
+		return new Student(	student.getID(), 
+				   	txtFirstName.getText(), 
+				   	txtLastName.getText(), 
+				   	txtAddress.getText(), 
+				   	txtCity.getText(),
+					txtState.getText(), 
+				   	txtZip.getText(), 
+				   	txtDOB.getText());
 	}
 }
